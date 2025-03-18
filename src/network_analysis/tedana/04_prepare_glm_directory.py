32350a726754078db960a7c3d8c8e576f711dd58
Logan Jordan Bennett <logben@sh02-01n58.int>
2025-03-18 12:07:33 -0700

    fix: cut 7 TRs from confounds

--- src/network_analysis/tedana/04_prepare_glm_directory.py
+++ src/network_analysis/tedana/04_prepare_glm_directory.py
@@ -1,18 +1,24 @@
-from pathlib import Path
-from glob import glob
-import shutil
-import pandas as pd
-import numpy as np
 import os
+import shutil
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
 
 def preprocess_events(df):
     df.fillna({'trial_type': 'na'}, inplace=True)
-    df['onset'] = df['onset'] - 7 * 1.49 
+    df['onset'] = df['onset'] - 7 * 1.49
     neg_rt = df['response_time'] < 0
     df['junk'] = np.where(neg_rt, 1, 0)
     df['na_trials'] = np.where((df['trial_type']=='na'), 1, 0)
     df['response_time'] = np.where(neg_rt, np.nan, df['response_time'])
     return df
+
+def preprocess_confounds(df):
+    # Remove the first 7 TRs
+    df = df.iloc[7:]
+    return df.reset_index(drop=True)
 
 def main():
     bids_dir = Path("./data/fMRI_data/")
@@ -27,21 +33,31 @@ def main():
     glm_dir = Path("./data/glm_data", f'sub-{subj_id}')
     glm_dir.mkdir(parents=True, exist_ok=True)
 
-    # Copy over all masks and confounds files 
+    # Copy over all masks and confounds files
     t1w_masks = sorted(subj_fmriprep_dir.glob("ses-*/func/*T1w*brain_mask.nii.gz"))
     mni_masks = sorted(subj_fmriprep_dir.glob("ses-*/func/*MNI*brain_mask.nii.gz"))
     confounds = sorted(subj_fmriprep_dir.glob("ses-*/func/*confounds*.tsv"))
     events = sorted(subj_bids_dir.glob("ses-*/func/*events.tsv"))
 
     # Move all the above files to the glm_dir
-    for file in t1w_masks + mni_masks + confounds:
-        rel_path = os.path.relpath(file, start=subj_bids_dir if file in events else subj_fmriprep_dir)
+    # Process masks
+    for file in t1w_masks + mni_masks:
+        rel_path = os.path.relpath(file, start=subj_fmriprep_dir)
         dest_path = glm_dir / rel_path
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
         shutil.copy2(file, dest_path)
 
+    # Process confounds files
+    for file in confounds:
+        df = pd.read_csv(file, sep='\t')
+        df = preprocess_confounds(df)
+        rel_path = os.path.relpath(file, start=subj_fmriprep_dir)
+        dest_path = glm_dir / rel_path
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        df.to_csv(dest_path, sep='\t', index=False)
+
+    # Process the events files
     for file in events:
-        # Process the events file
         df = pd.read_csv(file, sep='\t')
         df = preprocess_events(df)
         # Write out preprocessed events file
@@ -50,7 +66,7 @@ def main():
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
         df.to_csv(dest_path, sep='\t', index=False)
 
-    # Copy over all bold files 
+    # Copy over all bold files
     bold_files = sorted(subj_tedana_dir.glob("ses-*/func/*desc-optcom_bold.nii.gz"))
     for file in bold_files:
         rel_path = os.path.relpath(file, start=subj_tedana_dir)
@@ -62,61 +78,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# import glob
-# import pandas as pd
-# import shutil
-# import numpy as np
-# import os
-
-# bids = '/oak/stanford/groups/russpold/data/network_grant/validation_BIDS_old/'
-# fmriprep = bids+'derivatives/fmriprep/'
-# glm_dir = bids+'derivatives/glm_data/'
-# tedana = bids+'derivatives/tedana_kundu/'
-# # bids = '/oak/stanford/groups/russpold/data/network_grant/discovery_BIDS_21.0.1/'
-# # fmriprep = bids+'derivatives/fmriprep_21.0.1_c/'
-# # tedana = bids+'derivatives/tedana_kundu_c/'
-# # glm_dir = bids+'derivatives/glm_data_MNI/'
-# os.makedirs(glm_dir, exist_ok=True)
-# subjects = glob.glob(glm_dir+'sub-*')
-# subjects = [sub.split('/')[-1] for sub in subjects]
-# for sub in subjects:
-#     mni_mask = glob.glob(fmriprep+f'*{sub}/*/*/*task-*MNI*brain_mask.nii.gz')
-#     for mask in mni_mask:
-#         new_file = mask.replace(fmriprep, glm_dir)
-#         os.makedirs(os.path.dirname(new_file), exist_ok=True)
-#         shutil.copyfile(mask, new_file)
-#     confounds = sorted(glob.glob(fmriprep+f'*{sub}/*/*/*confounds*.tsv'))
-#     print("Confound files count: ", len(confounds))
-#     for file in confounds:
-#         df = pd.read_csv(file, sep='\t')
-#         df = df.iloc[7:]
-#         new_file = file.replace(fmriprep, glm_dir)
-#         os.makedirs(os.path.dirname(new_file), exist_ok=True)
-#         df.to_csv(new_file, sep='\t')
-
-#     events = sorted(glob.glob(bids+f'*{sub}/*/func/*events.tsv'))
-#     print("Event files count: ", len(events))
-#     for file in events:
-#         df = pd.read_csv(file, sep='\t')
-#         df['trial_type'].fillna('na', inplace=True)
-#         df['onset'] = df['onset']-7*1.49
-#         neg_rt = df['response_time'] < 0
-#         df['junk'] = np.where(neg_rt, 1, 0)
-#         df['na_trials'] = np.where((df['trial_type']=='na'), 1, 0)
-#         df['response_time'] = np.where(neg_rt, np.nan, df['response_time'])
-#         new_file = file.replace(bids, glm_dir)
-#         os.makedirs(os.path.dirname(new_file), exist_ok=True)
-#         df.to_csv(new_file, sep='\t', index=False)
-
-    # masks = sorted(glob.glob(fmriprep+f'*{sub}/*/*/*T1w*brain_mask.nii.gz'))
-    # print("Mask files count: ", len(masks))
-    # for file in masks:
-    #     new_file = file.replace(fmriprep, glm_dir)
-    #     os.makedirs(os.path.dirname(new_file), exist_ok=True)
-    #     shutil.copyfile(file, new_file)
-
-    # bolds = sorted(glob.glob(tedana+f'*{sub}/*/desc-optcom_bold.nii.gz'))
-    #bolds = sorted(glob.glob(tedana+f'*{sub}/*/desc-optcomDenoised_bold.nii.gz'))
